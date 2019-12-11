94170ca1ed33800dc5c63425f612674a60d08925
userwuqing <201453176@qq.com>
2019-12-11 19:34:06 +0800

    新增服务发现功能

--- Springcloud2019/spring-eureka-client/src/main/java/com/wuqing/controller/TestController.java
+++ Springcloud2019/spring-eureka-client/src/main/java/com/wuqing/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.wuqing.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * description：
@@ -22,10 +25,27 @@ public class TestController {
     @Autowired
     private RestTemplate restTemplate;
 
+    /**
+     * 服务发现
+     */
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("test.do")
     public String test(HttpServletRequest request, HttpServletResponse response){
 
         String comeBackValue = restTemplate.getForObject("http://eureka-server/test/test.do", String.class);
         return comeBackValue;
+    }
+
+    @GetMapping("discoveryServer.do")
+    public Object discoveryServer() {
+        List<String> services = discoveryClient.getServices();
+        System.out.println("所有启用服务暴露的微服务"+services);
+        List<ServiceInstance> instances = discoveryClient.getInstances("EUREKA-CLIENT");
+        for (ServiceInstance serviceInstance : instances) {
+            System.out.println(serviceInstance.getServiceId()+"\\s" +serviceInstance.getHost()+"\\s"+serviceInstance.getPort());
+        }
+        return this.discoveryClient;
     }
 }
--- Springcloud2019/spring-eureka-server-register/src/main/java/com/wuqing/Application.java
+++ Springcloud2019/spring-eureka-server-register/src/main/java/com/wuqing/Application.java
@@ -11,10 +11,13 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @date 2019年12月11日 15:13
  **/
 @SpringBootApplication
-//启用eureka服务注解
-@EnableEurekaServer
+@EnableEurekaServer //启用eureka服务注册中心
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    /**
+     * eureka自我保护机制：不会盲目注销不活跃的微服务（好死不如赖活着），保证高可用
+     */
 }
