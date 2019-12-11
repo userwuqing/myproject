e5176475ea1073cae4cf3ff02ea359698a08af9c
userwuqing <201453176@qq.com>
2019-12-11 19:16:40 +0800

    完善相应的配置信息

---
+++ Springcloud2019/spring-configuration-server/src/main/java/com/wuqing/Application.java
@@ -0,0 +1,19 @@
+package com.wuqing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * description：配置中心服务
+ *
+ * @author v_qqinwu
+ * @date 2019年12月11日 16:48
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
